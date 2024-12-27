92441155b64a03579ca3a6cb650bb24fd8bc1df2
Chip <chip22835ya@gmail.com>
2024-12-27 23:52:51 +0200

    init

--- crud/abstract.py
+++ crud/abstract.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm.sync import update
 
 
 class AbstractCrud:
@@ -25,7 +26,6 @@ class AbstractCrud:
         result = await session.execute(select(cls.model).where(cls.model.id == identification))
         return result.scalars().first()
 
-
     # @classmethod
     # async def read_with_filter(cls, session: AsyncSession, key:str, value:int):
     #     result = await session.execute(select(cls.model).where(cls.model.year > 5))
@@ -37,9 +37,24 @@ class AbstractCrud:
         results = await session.execute(select(cls.model))
         return results.scalars().all()
 
+    @classmethod
+    async def update(cls, session: AsyncSession, identification, schema):
+        updated_model = update(cls.model).where(cls.model == identification).values(**schema.model_dump(mode='json'))
+        try:
+            await session.execute(updated_model)
+            await session.commit()
+            return cls.read(session, identification)
+        except IntegrityError as e:
+            await session.rollback()
+            raise e
 
-    async def update(self):
-        pass
-
-    async def delete(self):
-        pass
+    @classmethod
+    async def delete(cls, session: AsyncSession, identification: int):
+        model_to_delete = delete(cls.model).where(cls.model == identification)
+        try:
+            await session.execute(model_to_delete)
+            await session.commit()
+            return {'status': 'Model was deleted'}
+        except IntegrityError as e:
+            await session.rollback()
+            raise e
